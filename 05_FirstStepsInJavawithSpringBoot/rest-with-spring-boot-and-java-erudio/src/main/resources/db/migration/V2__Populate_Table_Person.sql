b0b62624e070756d74b7963562b651e86e5cb5a7
João Paulo Nóbrega Figueiredo <joaopnobrega@gmail.com>
2023-11-09 19:48:50 -0300

    Migration with Flyway

---
+++ 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,9 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Sao Paulo', 'Ayrton', 'Male', 'Senna'),
+	(2, 'Anchiano - Italy', 'Leonardo', 'Male', 'da Vinci'),
+	(4, 'Ponbandar - India', 'Indira', 'Female', 'Gandhi'),
+	(5, 'Ponbandar - India', 'Mahatma', 'Male', 'Gandhi'),
+	(7, 'Kentucky - US', 'Muhamed', 'Female', 'Ali'),
+	(8, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+	(9, 'Smiljan - Croatia', 'Nikola', 'Male', 'Tesla');
+
